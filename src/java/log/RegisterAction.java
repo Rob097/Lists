fc8a8a4de4c554d3c9d5a042db51f2d4f03bfd3c
Roberto <dellantonio47@gmail.com>
2018-07-16 14:41:11 +0200

    try to add image display (not working)

--- src/java/log/RegisterAction.java
+++ src/java/log/RegisterAction.java
@@ -6,112 +6,102 @@
 package log;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
 
 /**
  *
  * @author Roberto97
  */
+@MultipartConfig(maxFileSize = 16177215)	// upload file's size up to 16MB
 public class RegisterAction extends HttpServlet {
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        Connection conn = null;
-            
-            try{
-                Class.forName("com.mysql.cj.jdbc.Driver");  // MySQL database connection
-                String dburl = "jdbc:mysql://sql2.freemysqlhosting.net:3306/sql2243047?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false";
-                String dbusername = "sql2243047";
-                String dbpassword = "mJ9*fQ4%";
-                conn = DriverManager.getConnection(dburl, dbusername, dbpassword);
-            
-            }catch(Exception e){
-                System.out.println("Errore di connessione RegisterAction");
-            }
-            
-            
-            
-            String username=null, nome=null, password=null, Tipostandard=null, TipononStandard=null, photo=null, standard="standard", nonStandard="nonStandard";    
-                username=request.getParameter("username"); //txt_username
-                nome=request.getParameter("nominativo"); //txt_name
-                password=request.getParameter("password"); //txt_password
-                Tipostandard=request.getParameter("standard"); //txt_standard
-                TipononStandard=request.getParameter("nonStandard"); //txt_nonSstandard
-                photo= request.getParameter("image");
-                System.out.println(photo);
-                if(photo.contains(".jpg") || photo.contains(".jpeg") || photo.contains(".png")){
-                    
-                }else{
-                    System.out.println("Attenzione serve un immagine! (jpg, jpeg, png)");
-                }
-                
-            try{
-                PreparedStatement pstmt=null; //create statement     
-                pstmt=conn.prepareStatement("insert into USER(Email,Password,Nominativo,Tipo,Image) values(?,?,?,?,?)"); //sql insert query
-                pstmt.setString(1,username);
-                pstmt.setString(2,password);
-                pstmt.setString(3,nome);
-                if(Tipostandard != null) {Tipostandard = "standard"; pstmt.setString(4,standard);}
-                else if(TipononStandard != null) {TipononStandard = "Nonstandard"; pstmt.setString(4,nonStandard);}
-                pstmt.setString(5,photo);
-                
+   
+	
+	// database connection settings
+	private String dbURL = "jdbc:mysql://sql2.freemysqlhosting.net:3306/sql2243047?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true&useSSL=false";
+	private String dbUser = "sql2243047";
+	private String dbPass = "mJ9*fQ4%";
+	
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// gets values of text fields
+		String username=null, nome=null, password=null, Tipostandard=null, TipononStandard=null, photo=null, standard="standard", nonStandard="nonStandard";
+		username=request.getParameter("username"); //txt_username
+        nome=request.getParameter("nominativo"); //txt_name
+        password=request.getParameter("password"); //txt_password
+        Tipostandard=request.getParameter("standard"); //txt_standard
+        TipononStandard=request.getParameter("nonStandard"); //txt_nonSstandard
+        InputStream inputStream = null;	// input stream of the upload file
+		
+		// obtains the upload file part in this multipart request
+		Part filePart = request.getPart("image");
+		if (filePart != null) {
+			// prints out some information for debugging
+			System.out.println(filePart.getName());
+			System.out.println(filePart.getSize());
+			System.out.println(filePart.getContentType());
+			
+			// obtains input stream of the upload file
+			inputStream = filePart.getInputStream();
+		}
+		
+		Connection conn = null;	// connection to the database
+		String message = null;	// message will be sent back to client
+		
+		try{
+			// connects to the database
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
 
-                pstmt.executeUpdate();
-                Cookie cookie = new Cookie("Nominativo", nome);
-                Cookie emailCookie = new Cookie("Email", username);
-                Cookie imageCookie = new Cookie("Image", photo);
-                Cookie typeCookie = null;   if(Tipostandard != null){
-                                                typeCookie = new Cookie("Type", Tipostandard);
-                                            }
-                                            else if(TipononStandard != null){
-                                                typeCookie = new Cookie("Type", TipononStandard);
-                                            }
-                Cookie logged = new Cookie("Logged", "on");
-                cookie.setMaxAge(60*60*24); imageCookie.setMaxAge(60*60*24); typeCookie.setMaxAge(60*60*24); emailCookie.setMaxAge(60*60*24); logged.setMaxAge(60*60*24);
-                response.addCookie(cookie); response.addCookie(imageCookie); response.addCookie(typeCookie); response.addCookie(emailCookie); response.addCookie(logged);
-                
-                request.getSession().setAttribute("Nominativo", nome);
-                request.getSession().setAttribute("Email", username);
-                request.getSession().setAttribute("Image", photo);
-                request.getSession().setAttribute("Type", typeCookie);
-                request.getSession().setAttribute("Logged", "on");
-                
-                //request.setAttribute("successMsg","Register Successfully...! Please login"); //register success messeage
-                if(Tipostandard != null) response.sendRedirect("Pages/standardType.jsp");
-                else if(TipononStandard != null) response.sendRedirect("Pages/notStandardType.jsp");
-                conn.close(); //close connection
-                
-            }catch(Exception e){
-                System.out.println("Errore database RegisterAction");
-                System.out.println(e);
-            }
+			// constructs SQL statement
+			String sql = "insert into USER(Email,Password,Nominativo,Tipo,Image) values(?,?,?,?,?)";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setString(1,username);
+                        statement.setString(2,password);
+                        statement.setString(3,nome);
+                        if(Tipostandard != null) {Tipostandard = "standard"; statement.setString(4,standard);}
+                        else if(TipononStandard != null) {TipononStandard = "Nonstandard"; statement.setString(4,nonStandard);}
+                        if (inputStream != null) {
+                        // fetches input stream of the upload file for the blob column
+                            statement.setBlob(5, inputStream);
+                        }   
+
+			// sends the statement to the database server
+			int row = statement.executeUpdate();
+			if (row > 0) {
+				message = "File uploaded and saved into database";
+			}
+		}catch (SQLException ex) {
+			message = "ERROR: " + ex.getMessage();
+			ex.printStackTrace();
+		}finally {
+			if (conn != null) {
+				// closes the database connection
+				try {
+					conn.close();
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+			// sets the message in request scope
+			response.sendRedirect("homepage.jsp");
+			
+			// forwards to the message page
+			//getServletContext().getRequestDispatcher("/Message.jsp").forward(request, response);
+		}
+	}
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
-}
